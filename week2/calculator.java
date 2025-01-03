30e1b7a60f9adb31c6d509d923029f330a7cbd50
ballisticdicts <jaydabholkar08@gmail.com>
2025-01-02 17:30:56 -0800

    Add calculator.java

---
+++ week2/calculator.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+class Calculator{
+    public double multiply(double num1, double num2){ 
+        return num1 * num2; 
+
+    }
+    public double divide(double num1, double num2){
+        return num1 / num2;
+    }
+    public double addition(double num1, double num2){
+        return num1 + num2;
+    }
+    public double subtraction(double num1, double num2){
+        return num1 - num2;
+    }
+}
+
+public class calculator{
+
+
+    
+}
+
+
+
+
